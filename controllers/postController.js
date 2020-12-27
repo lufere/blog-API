edcf7e8193a821c52bcdba8ea807eae670ee9135
Luis Rendon <luisfelipe.rendon@gmail.com>
2020-12-26 17:37:00 -0700

    Built Post creation controller

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,12 +1,35 @@
-var Post = require('../models/post');
+const Post = require('../models/post');
+const {body, validationResult} = require('express-validator');
 
 exports.post_list = (req, res, next)=>{
     res.send('Get all the posts');
 }
 
-exports.post_create = (req, res, next) => {
-    res.send('Create a new post');
-}
+exports.post_create = [
+    body('title', 'Title required').trim().escape().isLength({min:1}),
+    body('content', "Post content can't be empty").trim().escape().isLength({min:1}),
+    body('published').trim().escape(),
+
+    (req, res, next) =>{
+        const errors = validationResult(req);
+
+        const post = new Post({
+            title:req.body.title,
+            content:req.body.content,
+            timestamp: Date.now(),
+            author: req.user,
+            published: req.body.published,
+        });
+
+        if(!errors.isEmpty()) return res.status(400).json({post,errors:errors.array()});
+
+        post.save((err)=>{
+            if(err) return next(err);
+            res.json({post});
+        })
+        // res.send('Create a new post with user: ' +req.user.username);
+    }
+]
 
 exports.post_detail = (req, res, next) =>{
     res.send('Show the post with id: ' + req.params.id);
