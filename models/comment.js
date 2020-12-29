b6820de6602ffe0390b21aae15b4cd56a790c91c
Luis Rendon <luisfelipe.rendon@gmail.com>
2020-12-28 23:14:11 -0700

    Updated the Comment model

--- models/comment.js
+++ models/comment.js
@@ -5,7 +5,8 @@ var Schema = mongoose.Schema;
 var CommentSchema = new Schema({
     content: {type:String, required:true},
     timestamp: {type:Date, required:true},
-    Author: {type:Schema.Types.ObjectId, ref: 'User'},
+    author: {type:Schema.Types.ObjectId, ref: 'User'},
+    post: {type:Schema.Types.ObjectId, ref: 'Post'},
 })
 
 module.exports = mongoose.model('Comment', CommentSchema);
