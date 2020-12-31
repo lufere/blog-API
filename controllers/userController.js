88b89456e49b091a44d571d9dd2ee52c8048d9f4
Luis Rendon <luisfelipe.rendon@gmail.com>
2020-12-30 21:13:35 -0700

    Built the User update and User delete controllers

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,8 @@
 var User = require('../models/user');
+const bcrypt = require('bcryptjs');
+
+const {body, validationResult} = require('express-validator');
+const user = require('../models/user');
 
 exports.user_list = (req, res, next) => {
     User.find({})
@@ -16,10 +20,49 @@ exports.user_detail = (req, res, next) => {
         .catch(err=>next(err));
 }
 
-exports.user_update = (req, res, next) => {
-    res.send('Update user with id: ' + req.params.id);
-}
+exports.user_update = [
+    body('username').trim().escape().isLength({min:1}),
+    body('creator').trim().escape(),
+    (req, res, next) => {
+        const errors = validationResult(req);
+        var user;
+        User.findById(req.params.id)
+            .then(foundUser=>{
+                if(!req.body.password){
+                    user = new User({
+                        username: req.body.username,
+                        password: foundUser.password,
+                        creator: req.body.creator,
+                        _id: req.params.id,
+                    })
+                    if(!errors.isEmpty()) res.json({errors:errors.array()})
+                    User.findByIdAndUpdate(req.params.id,user,{new:true})
+                        .then(updatedUser=>res.json({updatedUser}))
+                        .catch(err=>next(err));
+                }else{
+                    bcrypt.hash(req.body.password,10)
+                        .then(hashedPassword=>{
+                            console.log(req.body.username);
+                            user = new User({
+                                username: req.body.username,
+                                password:hashedPassword,
+                                creator:req.body.creator,
+                                _id: req.params.id,
+                            })
+                            if(!errors.isEmpty()) res.json({errors:errors.array()})
+                            User.findByIdAndUpdate(req.params.id,user,{new:true})
+                                .then(updatedUser=>res.json({updatedUser}))
+                                .catch(err=>next(err));
+                        })
+                        .catch(err=>next(err));
+                }
+            })
+            .catch(err=>next(err));
+    }
+]
 
 exports.user_delete = (req, res, next) => {
-    res.send('Delete user with id: ' + req.params.id);
+    User.findByIdAndDelete(req.params.id)
+        .then(deletedUser=>res.json({deletedUser}))
+        .catch(err=>next(err));
 }
